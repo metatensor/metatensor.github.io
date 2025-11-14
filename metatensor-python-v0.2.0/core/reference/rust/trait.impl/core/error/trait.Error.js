d5eeefdf262372cd63100f89196405699fe9fd89
Luthaf <Luthaf@users.noreply.github.com>
2025-11-14 15:26:16 +0000

    deploy: metatensor/metatomic@c1bc1b816b78d503089f74fc3f2997e473665c73

---
+++ metatensor-python-v0.2.0/core/reference/rust/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"metatensor":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"metatensor/errors/struct.Error.html\" title=\"struct metatensor::errors::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
