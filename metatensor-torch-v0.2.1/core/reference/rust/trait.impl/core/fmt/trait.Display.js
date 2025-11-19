8a323d241de17dc2df396f201aff0f7b8f25f77e
Luthaf <Luthaf@users.noreply.github.com>
2025-11-19 08:09:31 +0000

    deploy: metatensor/metatomic@ecbf681f90ee5dbd921d5420099c0fdf0e5dab2d

---
+++ metatensor-torch-v0.2.1/core/reference/rust/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"metatensor":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"metatensor/errors/struct.Error.html\" title=\"struct metatensor::errors::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"metatensor/struct.LabelValue.html\" title=\"struct metatensor::LabelValue\">LabelValue</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
